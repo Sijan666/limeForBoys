acd0e559a1c81e70df735d204e194eef9eb1aa76
Sijan666 <666majharulislam@gmail.com>
2025-12-18 20:29:40 +0600

    Nayeem Final

--- src/components/layouts/TheApp.jsx
+++ src/components/layouts/TheApp.jsx
@@ -1,52 +1,52 @@
-
-import Heading from "../Heading"
-import Image from "../Image"
-import picOne from "../../assets/loacte.png"
-import picTwo from "../../assets/scane.png"
-import picThree from "../../assets/ride.png"
-import Container from "../Container"
+import Heading from "../Heading";
+import Image from "../Image";
+import picOne from "../../assets/loacte.png";
+import picTwo from "../../assets/scane.png";
+import picThree from "../../assets/ride.png";
+import Container from "../Container";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import Flex from "../Flex"
-
-
-
+import Flex from "../Flex";
 
 const TheApp = () => {
-      var settings = {
+  var settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    
   };
-    return (
-        <>
-      
-      <section className="bg-[#EEEEEE] py-30">
-        <Heading textOne={"The App"} textTwo={" How to Lime "} className={" text-2xl"}/>
+  return (
+    <>
+      <section className="bg-[#EEEEEE] py-30 font-poppins">
+        <Heading
+          textOne={"The App"}
+          textTwo={" How to Lime "}
+          className={" text-2xl"}
+        />
+        <div className="text-center">
+          <h4 className="text-[#00B200] text-[20px] ">The App</h4>
+          <h2 className="text-[40px] pb-16">How to Lime</h2>
+        </div>
         <Container>
-      <div >
-        {/* <Flex className=" gap-2.5"> */}
-        <Slider {...settings}>
-           <Image imgSrc={picOne}/>
-         <Image imgSrc={picTwo}/>
-         <Image imgSrc={picThree}/>
-         <Image imgSrc={picOne}/>
-          <Image imgSrc={picTwo}/>
-          <Image imgSrc={picThree}/>
-         </Slider>
-      {/* </Flex> */}
-      </div>
+          <div>
+            {/* <Flex className=" gap-2.5"> */}
+            <Slider {...settings}>
+              <Image imgSrc={picOne} />
+              <Image imgSrc={picTwo} />
+              <Image imgSrc={picThree} />
+              <Image imgSrc={picOne} />
+              <Image imgSrc={picTwo} />
+              <Image imgSrc={picThree} />
+            </Slider>
+            {/* </Flex> */}
+          </div>
         </Container>
       </section>
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default TheApp
-
-
+export default TheApp;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../layouts/Banner";
 import Locations from "../layouts/Locations"
 import NewsBlog from "../layouts/NewsBlog"
+import TheApp from "../layouts/TheApp";
 
 
 const Home = () => {
     return (
         <>
         <Banner/>
+        <TheApp/>
         <Locations/>
         <NewsBlog/>
         </>
