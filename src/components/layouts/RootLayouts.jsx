5c3fef6af561d1bfaa74caeb405da138f308dddc
Md Jaber Hossain <md9292463@gmail.com>
2025-12-19 21:28:46 +0600

    add

--- src/components/layouts/RootLayouts.jsx
+++ src/components/layouts/RootLayouts.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom"
 import Home from "../pages/Home"
+import Footer from "./Footer"
 
 
 const RootLayouts = () => {
     return (
         <>
         <Home/>
+        <Outlet/>
+        <Footer/>
         </>
     )
 }
