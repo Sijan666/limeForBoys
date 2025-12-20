91b926c9c7e4551c99b95df68743b05aba4ec1a8
Sijan666 <666majharulislam@gmail.com>
2025-12-20 17:43:25 +0600

    Add Map component and integrate it into Home page layout

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Locations from "../layouts/Locations"
 import NewsBlog from "../layouts/NewsBlog"
 import Whylime from "../layouts/Whylime"
-import Map from "./Map"
+import Map from "../layouts/Map"
+import TheApp from "../layouts/TheApp"
 
 
 const Home = () => {
     return (
         <>
+        <TheApp/>
         <Locations/>
         <NewsBlog/>
         <Whylime/>
