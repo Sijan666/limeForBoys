917001b71bbdf9765b6f1eb77169ddafcb352dd1
Sijan666 <666majharulislam@gmail.com>
2025-12-20 22:30:59 +0600

    P images

--- src/components/layouts/Map.jsx
+++ src/components/layouts/Map.jsx
@@ -1,18 +1,17 @@
 import Container from "../Container";
 import React from "react";
+import Images from "../Images";
+import appstore from '../../assets/appstore.png'
+import google from '../../assets/google.png'
+import map from '../../assets/map.png'
 
 const Map = () => {
   return (
     <div className="bg-[#1A1A1A] py-[232px]">
       <Container>
   <div className="flex items-center gap-20">
-    
     {/* LEFT: Map image */}
-    <img
-      src="/src/assets/map.png"
-      alt="Map"
-      className="w-[600px] h-auto"
-    />
+    <Images imgSrc={map} className={'w-[600px] h-auto'}/>
 
     {/* RIGHT: Download card */}
     <div className="backdrop w-[440px] h-[600px] ml-40">
@@ -21,16 +20,8 @@ const Map = () => {
       </h1>
 
       <div className="flex gap-4 mt-10">
-        <img
-          src="/src/assets/appstore.png"
-          alt="App Store"
-          className="w-[180px] h-auto object-contain"
-        />
-        <img
-          src="/src/assets/google.png"
-          alt="Google Play"
-          className="w-[180px] h-auto object-contain"
-        />
+        <Images imgSrc={appstore} className={'w-[180px] h-auto object-contain'}/>
+        <Images imgSrc={google} className={'w-[180px] h-auto object-contain'}/>
       </div>
     </div>
 
