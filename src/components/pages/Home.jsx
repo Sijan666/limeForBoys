f2ec64e683156c287b549c3dcc1384933d7d2618
Sijan666 <666majharulislam@gmail.com>
2025-12-20 17:45:04 +0600

    Reorder components in Home page layout for improved structure

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,15 +3,17 @@ import NewsBlog from "../layouts/NewsBlog"
 import Whylime from "../layouts/Whylime"
 import Map from "../layouts/Map"
 import TheApp from "../layouts/TheApp"
+import Banner from "../layouts/Banner"
 
 
 const Home = () => {
     return (
         <>
+        <Banner/>
         <TheApp/>
         <Locations/>
-        <NewsBlog/>
         <Whylime/>
+        <NewsBlog/>
         <Map/>
         </>
     )
